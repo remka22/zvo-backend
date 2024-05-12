a4be3a26d575a819387c0953b72a030b8fd63c54
Михаил Пилипенко <pilipenkohit433@gmail.com>
2024-05-12 20:46:48 +0800

    fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $role = ['Администратор', 'Студент', 'Преподаватель', 'Методист', 'Директор'];
+        foreach ($role as $r) {
+            Role::create([
+                'name' => $r,
+            ]);
+        }
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create([
+            'role_id' => 1,
+            'fio' => 'Админ',
+            'email' => 'admin',
+            'password' => bcrypt('tasar232'),
+            'mira_id' => 1
+        ]);
     }
 }
--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -16,16 +16,16 @@ class TaskSeeder extends Seeder
      */
     public function run(): void
     {
-        $m_course = MoodleCourse::all();
-        foreach($m_course as $mc){
-            for ($i=0; $i < rand(3,7); $i++) { 
-                $m_course = new MoodleTask;
-                $m_course->link_id = rand(63244, 85324);
-                $m_course->name = "Задание $i";
-                $m_course->type = "assign";
-                $m_course->course_id = $mc->id;
-                $m_course->save();
-            }
-        }
+        // $m_course = MoodleCourse::all();
+        // foreach($m_course as $mc){
+        //     for ($i=0; $i < rand(3,7); $i++) { 
+        //         $m_course = new MoodleTask;
+        //         $m_course->link_id = rand(63244, 85324);
+        //         $m_course->name = "Задание $i";
+        //         $m_course->type = "assign";
+        //         $m_course->course_id = $mc->id;
+        //         $m_course->save();
+        //     }
+        // }
     }
 }
